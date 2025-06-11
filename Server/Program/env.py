d5b979488fb468defd4c9a1727835e77a76e262d
LuquyPest <lylianfredon@gmail.com>
2025-06-11 14:26:54 +0200

    no-docker

---
+++ Server/Program/env.py
@@ -0,0 +1,12 @@
+LDAPUSER = "RF-AD\RO.RF-AD"
+LDAPPASS = "AD-password"
+LDAP_SERVER = "ldap://ip-AD"
+DOOR_ACCESS_GROUPS_DN = "OU=DOORS,DC=RF-AD,DC=com"
+USERS_DN = "OU=UTILISATEURS,DC=RF-AD,DC=com"
+WebServerPORT = 5000
+DBFILE = {
+    "host": "localhost",
+    "user": "root",
+    "password": "Password",
+    "database": "controle_acces"
+}
